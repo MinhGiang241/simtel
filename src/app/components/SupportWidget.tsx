bf164934ff2049a33a5350ae37a267b999ca8edd
Hai Duc <haiduczxzx@gmail.com>
2023-12-06 14:52:11 +0700

    link live chat

--- src/app/components/SupportWidget.tsx
+++ src/app/components/SupportWidget.tsx
@@ -6,20 +6,15 @@ import { Button } from 'antd'
 import Zl from './logo/zl.svg'
 import Mss from './logo/mss.svg'
 import Tel from './logo/tel.svg'
+import Link from 'next/link'
 
 
 export default function SupportWidget() {
   return (
     <div className='bottom-0 right-0 fixed flex flex-col z-50' >
-      <button>
-        <Mss />
-      </button>
-      <button>
-        <Zl />
-      </button>
-      <button>
-        <Tel />
-      </button>
+      <Link target='_blank' href='https://www.facebook.com/reddichonsodep?locale=vi_VN'><Mss /></Link>
+      <Link target='_blank' href='https://zalo.me/g/mnswtd365'><Zl /></Link>
+      <Link target='_blank' href='tel:+84559111666'><Tel /></Link>
     </div>
   )
 }
