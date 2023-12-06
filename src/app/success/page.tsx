881664581b3379732af1422f81d39a0469bf8c19
Hai Duc <haiduczxzx@gmail.com>
2023-12-06 11:38:10 +0700

    modal

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,16 +1,16 @@
 "use client";
-import React, { Dispatch, useEffect } from "react";
+import React, { Dispatch, useEffect, useState } from "react";
 import PageWrapper from '../components/pageWrapper';
 import { Modal } from 'antd';
 import { useSearchParams } from 'next/navigation'
 import Clipboard from 'clipboard';
 import {
-    CheckCircleFilled,
+    CheckCircleFilled, CopyOutlined
 } from "@ant-design/icons";
 import toast from "react-hot-toast";
 
-
 export default function Page() {
+    const [isModalOpen, setIsModalOpen] = useState(true);
     const searchParams = useSearchParams()
     const seriCard = searchParams.get('seriCard')
     const code = searchParams.get('order_code')
@@ -24,9 +24,13 @@ export default function Page() {
         }
     };
 
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <PageWrapper>
-            <Modal open={true} >
+            <Modal open={isModalOpen} onCancel={handleCancel} footer={null}>
                 <div className='flex flex-col items-center'>
                     <CheckCircleFilled
                         className="text-5xl mb-3 text-green-600"
@@ -35,7 +39,7 @@ export default function Page() {
                     <div className='font-light'>Quý khách đã thanh toán thành công cho đơn hàng:</div>
                     <div>{code}</div>
                     <div className='font-light'>Mã thẻ của bạn là:</div>
-                    <div className='flex justify-between px-5 items-center border bg-m_gray w-60 m-auto rounded-lg'>{seriCard}<button onClick={handleCopyToClipboard}>Copy</button></div>
+                    <div className='flex justify-between pl-6 pr-2 items-center border bg-m_gray w-60 m-auto rounded-lg'>{seriCard}<button onClick={handleCopyToClipboard}><CopyOutlined /></button></div>
                 </div>
             </Modal>
         </PageWrapper>
